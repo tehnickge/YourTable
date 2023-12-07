63b1a81d2597df4b5b78b58417031e1cd9efdbc9
tehnickge <71207932+tehnickge@users.noreply.github.com>
2023-12-07 20:08:49 +0300

    ADD AUTH
    ADD AUTH, const user, samy disign

--- your-table/configs/auth.ts
+++ your-table/configs/auth.ts
@@ -8,6 +8,37 @@ export const authOptions : AuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+          }),
+          GitHubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!
+          }),
+          CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: {
+                    label: "Username:",
+                    type: "text",
+                    placeholder: "Enter your name"
+                },
+                password: { 
+                    label: "Password",
+                    type: "password"
+                }
+            },
+            async authorize(credentials, req) {
+                //tut budet vzaimodestvie s bd 
+                // Add logic here to look up the user from the credentials supplied
+                const user = { id: "1", name: "tehnick", password: "123"}
+                if (user) {
+                    // Any object returned will be saved in `user` property of the JWT
+                    return user
+                  } else {
+                    // If you return null then an error will be displayed advising the user to check their details.
+                    return null
+                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+                  }
+            }
           })
       ]
 }
