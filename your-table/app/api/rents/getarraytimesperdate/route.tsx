8b0153369d0c5c63d35f332ab192ea701d8521f7
tehnickge <71207932+tehnickge@users.noreply.github.com>
2023-12-24 02:01:57 +0300

    add
    .

--- your-table/app/api/rents/getarraytimesperdate/route.tsx
+++ your-table/app/api/rents/getarraytimesperdate/route.tsx
@@ -1,11 +1,75 @@
+import prisma from "@/lib/prisma";
 import moment from "moment";
 import { NextResponse } from "next/server";
 
+const generateTimeArray = async(timeStart : any, timeEnd : any, date: any, restId : any, slotId : any) => {
+    if(moment.utc(timeStart, "HHmm").isAfter(moment.utc(timeEnd,"HHmm"))) { return NextResponse.json({"error" : "bad schedule"})}
+
+    const RentPerDay = await prisma.rent.findMany({
+       where: {
+        restaurantId: restId,
+        slots: {
+            some: {
+                slotId: slotId
+            }
+        }
+       },
+       include: {
+        slots: true
+       }
+    });
+    const checkToDay = await RentPerDay.filter((rent)=> {
+        return(moment.utc(rent.timeStart,"YYYY-MM-DD").format("YYYY-MM-DD") === moment(date,"YYYY-MM-DD").format("YYYY-MM-DD"))
+    })
+  
+    if(checkToDay.length !== 0) { return []}
+
+    let tmpTime = await moment.utc(timeStart,"HHmm");
+
+    // if this date = rent day
+    if(moment(date, "YYYY-MM-DD").isSame(moment.utc().format("YYYY-MM-DD"))) {
+        //@ts-ignore
+        tmpTime = await moment.utc().format("HH");
+    } else {
+        tmpTime = await moment.utc(timeStart,"HHmm");
+    }
+
+    const arrTimes = [];
+
+    while(moment.utc(timeEnd,"HHmm").isSameOrAfter(moment.utc(tmpTime,"HHmm"))) {
+        arrTimes.push(moment(tmpTime, "HHmm").format("HH:mm"));
+        tmpTime = await moment(tmpTime, "HHmm").add(1,"h")
+    } 
+    return await arrTimes
+}
+
 export async function POST(req: Request) {
     const body = await req.json()
-    console.log(body);
     const dateNow = moment().format("YYYY-MM-DD");
-    if(moment(body.startDate, "YYYY-MM-DD").isBefore(dateNow)) { return NextResponse.json({"error" : "not valid date"})}
+    if(moment(body.startDate, "YYYY-MM-DD").isBefore(dateNow)) { return NextResponse.json({"error" : "not valid date"})};
 
-    return NextResponse.json({"kek" : "lol"})
+    const restaurant = await prisma.restaurant.findUnique({
+        where: {
+            id: body.restaurantId
+        },
+        include: {
+            workSchedulePerDay: {
+                include: {
+                    day: true
+                }
+            },
+        },
+    })
+
+    if(!restaurant) { return NextResponse.json({"error": "restaurant not founded"})};
+
+    
+    const thisDayOfWeek = await restaurant.workSchedulePerDay.filter((shedule : any) => {
+        return shedule.day.title === moment.utc(body.startDate,"YYYY-MM-DD").format("dddd")
+    })
+    console.log(thisDayOfWeek);
+    const arrWithTime = await generateTimeArray(thisDayOfWeek[0].timeBegin,thisDayOfWeek[0].timeEnd, body.startDate, body.restaurantId, body.selectedSlot)
+    console.log(await arrWithTime)
+
+    return NextResponse.json(arrWithTime);
 }
--- your-table/components/Restaurant/RestaurantZonesAndSlots.tsx
+++ your-table/components/Restaurant/RestaurantZonesAndSlots.tsx
@@ -3,14 +3,12 @@
 import { Button, Fab, Icon, Paper, Typography } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import { Slot, Zone } from "@prisma/client";
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import TableBarIcon from "@mui/icons-material/TableBar";
 import moment from "moment";
-
-const arrayTime = ["6:00", "7:00", "8:00", "9:00", "10:00","11:00"]
-
+import zIndex from "@mui/material/styles/zIndex";
 
 export default function RestaurantZonesAndSlots(props: PropsWithChildren) {
   const [startDate, setStartDate] = useState(new Date());
@@ -23,40 +21,42 @@ export default function RestaurantZonesAndSlots(props: PropsWithChildren) {
   const [arrayTimeEnd, setArrayTimeEnd] = useState([]);
 
   const getFreeTimePerDate = () => {
+    
     const dataToFetch = {
       "startDate" : moment(startDate).format("YYYY-MM-DD"), 
       "selectedSlot" : selectedSlot,
-      "restaurantId": props.zonesAndSlots.id,
-      "timeBegin": selectedTimeBegin
+      "restaurantId": props.zonesAndSlots.id
     }
 
     if(dataToFetch.restaurantId === null
       || dataToFetch.selectedSlot === null
       || dataToFetch.startDate === null
-      || dataToFetch.timeBegin === null
       ) { return console.log("ERROR DATA YEMPTY")}
 
     fetch("http://localhost:3000/api/rents/getarraytimesperdate", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(dataToFetch),
-    }).then((res: any) => res.json()).then((data) => console.log(data))
+    }).then((res: any) => res.json()).then((data: any) => setArrayTimeBegin([...data]))
 
   }
 
   const selectTimeBeginHandler = (e: any) => {
+    // @ts-ignore
     setSelectedTimeBegin((prev: any) => {
       return moment(e.target.id,"HHmm").format("HH:mm:ss")
     })
   }
   
   const selectTimeEndHandler = (e: any) => {
+    // @ts-ignore
     setSelectedTimeEnd((prev: any) => {
       return moment(e.target.id,"HHmm").format("HH:mm:ss")
     })
   }
 
   const selectSlotHandler = (e: any) => {
+    // @ts-ignore
     setSelectedSlot((prev: any) => {
       return Number(e.target.id);
     });
@@ -67,6 +67,21 @@ export default function RestaurantZonesAndSlots(props: PropsWithChildren) {
       return !prev;
     });
   };
+
+  const datePickerChangeHandler = (data : any) => {
+    setStartDate(data);
+  }
+
+  useEffect(() => {
+    const filtered = arrayTimeBegin.filter((time) => { return moment(time,"HHmm").isAfter(moment(selectedTimeBegin,"HHmm")) });
+    console.log("filtered",filtered);
+    setArrayTimeEnd([...filtered]);
+  },[selectedTimeBegin]);
+
+  useEffect(()=> {
+    getFreeTimePerDate();
+  },[selectedSlot, startDate])
+
 
   console.log(selectedSlot);
   console.log(selectedTimeBegin);
@@ -81,6 +96,14 @@ export default function RestaurantZonesAndSlots(props: PropsWithChildren) {
       </Fab>
       {isMenuSlotsOpen && (
         <Paper>
+          <Grid2>
+            <Typography>Дата:</Typography>
+            <DatePicker
+              selected={startDate}
+              onChange={datePickerChangeHandler}
+              sx={{zIndex: 50}}
+            />
+          </Grid2>
           {props.zonesAndSlots.zones.map((zone: Zone) => {
             return (
               <Grid2 key={zone.id} sx={{ marginLeft: 1, marginRight: 1 }}>
@@ -99,13 +122,6 @@ export default function RestaurantZonesAndSlots(props: PropsWithChildren) {
               </Grid2>
             );
           })}
-          <Grid2>
-            <Typography>Дата:</Typography>
-            <DatePicker
-              selected={startDate}
-              onChange={(date: any) => setStartDate(date)}
-            />
-          </Grid2>
         </Paper>
       )}
       { isMenuSlotsOpen && ((selectedSlot === null) ? 
@@ -114,13 +130,13 @@ export default function RestaurantZonesAndSlots(props: PropsWithChildren) {
        <Paper>
         <Grid2>
         <Typography>Время начала:</Typography>
-          {arrayTime.map((time) => {
+          {arrayTimeBegin.map((time) => {
             return (<Button key={time} id={`${time}`} onClick={selectTimeBeginHandler} >{time}</Button>)
           })}
         </Grid2>
         <Grid2>
         <Typography>Время конца:</Typography>
-        {arrayTimeBegin.map((time) => {
+        {arrayTimeEnd.map((time) => {
             return (<Button key={time} id={`${time}`} onClick={selectTimeEndHandler} >{time}</Button>)
           })}
         </Grid2>
